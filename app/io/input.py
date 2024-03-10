c3325cdf0df83cb18d76bef386ec6a5668873cee
bohdan <howchromium@gmail.com>
2024-03-10 21:16:03 +0200

    name with inputs of the functions declared

---
+++ app/io/input.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def input_text() -> str:
+    pass
+
+
+def read_file(filename: str) -> str:
+    pass
+
+
+def read_csv_with_pandas(filename: str) -> str:
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,15 @@
+from typing import Dict
+import pandas as pd
+
+
+def output_text() -> str:
+    pass
+
+
+def write_file(content: str) -> None:
+    pass
+
+
+def write_csv_with_pandas(dictionary: Dict[int, str]) -> None:
+    pass
+
