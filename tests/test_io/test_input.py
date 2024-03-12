5461777f853eb91aaaf210318c09633bb009d1d9
bohdan <howchromium@gmail.com>
2024-03-12 10:33:57 +0200

    created inheriting class for testing

---
+++ tests/test_io/test_input.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import patch
+import pandas as pd
+from app.io.input import input_text, read_file, read_csv_with_pandas
+
+
+class InputFunctionsTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
